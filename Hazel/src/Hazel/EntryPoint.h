b4aece4f4425f12e148507f5a892fd7726fe27f5
Shivasankar-1996 <v.shiva4@gmail.com>
2024-01-20 22:31:39 +0530

    Log wrapper added. (spdlog)

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,11 +1,19 @@
 #pragma once
 #ifdef HZ_WINDOWS_PLATFORM
 
-//extern Hazel::Application* Hazel::CreateApplication();
+extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+	
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+
+	
 	auto app = Hazel::CreateApplication();
 	app->run();
+	delete app;
 
 	return 0;
 }
