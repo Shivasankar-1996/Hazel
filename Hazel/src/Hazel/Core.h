244c1b467bb12b3701ea0c2bf0ce5facc45c9af7
Shivasankar-1996 <v.shiva4@gmail.com>
2024-01-22 21:42:52 +0530

    Added test event

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -8,3 +8,5 @@
 #else
 	#error Hazel onyl supports Windows 
 #endif
+
+#define BIT(x) (1 << x)
--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -2,6 +2,8 @@
 
 #include"spdlog/spdlog.h"
 
+#include "spdlog/fmt/ostr.h"
+
 #include<memory>
 
 #include "Core.h"
